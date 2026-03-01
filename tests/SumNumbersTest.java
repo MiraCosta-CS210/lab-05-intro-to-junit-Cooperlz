10115b80354ce886a4bcab19efc0ad1b4e3c26ea
Cooper Zvegintzov <Cooperzvegintzov@gmail.com>
2026-03-01 12:51:35 -0800

    Ready to grade!

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,19 @@
-public class SumNumbersTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SumNumbersTest {
+
+    @Test
+    void sumIsEvenTest1() {
+        SumNumbers sum = new SumNumbers();
+        int result = sum.sumIsEven(1,1);
+        assertEquals(1,result);
+    }
+
+    @Test
+    void sumIsEvenTest2() {
+        SumNumbers sum = new SumNumbers();
+        int result = sum.sumIsEven(1,2);
+        assertEquals(0,result);
+    }
 }
--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,26 @@
-public class TipCalculatorTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TipCalculatorTest {
+
+
+    @Test
+    void calculateTipTest1() {
+        TipCalculator tipCalculator = new TipCalculator();
+        double t1 = tipCalculator.calculateTip(100.00,20);
+
+        assertEquals(20.00,t1);
+
+    }
+
+
+    @Test
+    void calculateTipTest2() {
+        TipCalculator tipCalculator = new TipCalculator();
+        double t1 = tipCalculator.calculateTip(150.00,10);
+
+        assertEquals(15.00,t1);
+
+    }
+
 }
